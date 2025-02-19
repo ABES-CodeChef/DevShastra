37a662dfbd9b22b29c75af745de1c175acebe2ef
rohit bhardwaj <rb8329563@gmail.com>
2025-02-19 17:28:50 +0530

    initialize React app with Vite and Tailwind CSS and react router dom

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-blue-500 text-white text-center text-4xl">DevShastra</h1>
+    </div>
+  );
+}
+
+export default App;
